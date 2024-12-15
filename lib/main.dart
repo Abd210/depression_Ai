9c5094ca8f22b7eb93a91e44014370e4b7fc1316
Abd210 <150333656+Abd210@users.noreply.github.com>
2024-12-15 17:28:57 +0200

    Prepare for GitHub Pages deployment

--- lib/main.dart
+++ lib/main.dart
@@ -2,13 +2,6 @@ import 'dart:convert';
 import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
-import 'package:firebase_core/firebase_core.dart';
-import 'firebase_options.dart';
-
-
-await Firebase.initializeApp(
-options: DefaultFirebaseOptions.currentPlatform,
-);
 void main() {
   runApp(const MyApp());
 }
